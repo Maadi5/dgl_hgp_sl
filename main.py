83f8679500651491ada4b46edaefa8dc80ee2fec
Maadi5 <ma.adithya55@gmail.com>
2022-08-23 01:53:24 +0530

    loss change

--- main.py
+++ main.py
@@ -107,8 +107,6 @@ def train(model: torch.nn.Module, optimizer, trainloader, device):
         #out = model(batch_graphs, n_feat = batch_graphs.ndata["feat"], e_feat = None)   #change for dgl
         out = model(batch_graphs, n_feat=batch_graphs.ndata["features"]) #e_feat is edge_weights. not features
         #loss = F.nll_loss(out, batch_labels)
-        print('out: ', out.shape)
-        print('labels: ', batch_labels.shape)
         loss = criterion(out.to(dtype=torch.float32), batch_labels.to(dtype= torch.float32))
 
         loss.backward()
@@ -142,6 +140,8 @@ def test(model: torch.nn.Module, loader, device, num_classes):
         #loss += F.nll_loss(out, batch_labels, reduction="sum").item()
         #loss = F.nll_loss(out, batch_labels)
         loss = criterion(out.to(dtype=torch.float32), batch_labels.to(dtype=torch.float32))
+        print('out: ', out.shape)
+        print('labels: ', batch_labels.shape)
         correct += pred.eq(batch_labels).sum().item()
     confusion = confusion_matrix(labels_all, pred_all)
     pr_recall = precision_recall(preds= torch.tensor(pred_all), target= torch.tensor(labels_all), average='macro', mdmc_average=None, ignore_index=None,
