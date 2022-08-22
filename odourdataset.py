3678c8e2719244deae2431d8d80c6a73d4a54bed
Maadi5 <ma.adithya55@gmail.com>
2022-08-23 01:54:50 +0530

    loss change

--- odourdataset.py
+++ odourdataset.py
@@ -400,7 +400,8 @@ class OdourDataset_test(DGLDataset):
         for idx, row in df.iterrows():
             if row['IsomericSMILES'] != '':
                 mol = molecule_from_smiles(row['IsomericSMILES'])
-                label = [int(i) for i in list(row)[3:-3]]
+                print(list(row))
+                label = [int(i) for i in list(row)[3:-2]]
                 atom_features, bond_features, pair_indices, num_nodes = graph_from_molecule(mol, global_node=True)
                 g = create_dgl_graph(pair_indices, num_nodes=num_nodes)
                 g.ndata['features'] = torch.from_numpy(np.array(atom_features, dtype=np.float32))
