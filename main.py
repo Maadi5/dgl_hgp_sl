f092c87db8acb53c64fdafc3aa59ecb91e1ab0ba
Maadi5 <ma.adithya55@gmail.com>
2022-08-23 16:58:06 +0530

    some shit

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def test(model: torch.nn.Module, loader, device, num_classes):
             l_list = []
             idd = np.where(b.cpu().numpy() == 1)[0]
             for bb in idd:
-                l_list.append(bb)
+                l_list.append(id2label[str(bb)])
             label_list.append(l_list)
         for each_t in sig_out:
             p_list = []
@@ -161,9 +161,10 @@ def test(model: torch.nn.Module, loader, device, num_classes):
         #loss += F.nll_loss(out, batch_labels, reduction="sum").item()
         #loss = F.nll_loss(out, batch_labels)
         loss = criterion(out.to(dtype=torch.float32), batch_labels.to(dtype=torch.float32))
+        print (loss)
         # print('out: ', out.shape)
         # print('labels: ', batch_labels.shape)
-        correct += pred.eq(batch_labels).sum().item()
+        #correct += pred.eq(batch_labels).sum().item()
     confusion = confusion_matrix(labels_all, pred_all)
     pr_recall = precision_recall(preds= torch.tensor(pred_all), target= torch.tensor(labels_all), average='macro', mdmc_average=None, ignore_index=None,
                                  num_classes=num_classes, threshold=0.5, top_k=None, multiclass=None)
