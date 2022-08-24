bc1f61ba759a933e2b947d689dffa9b576b1d861
Maadi5 <ma.adithya55@gmail.com>
2022-08-25 00:37:28 +0530

    trying some iterativeshit for split

--- odourdataset.py
+++ odourdataset.py
@@ -276,7 +276,7 @@ class OdourDataset_train(DGLDataset):
 
     def process(self):
         savepath = '/content/drive/MyDrive/dgl_hgp_sl/dataset/'
-        df = pd.read_csv(os.path.join(savepath, ('odour_graphs_leff_' + str('train') + '.csv')))#, index_col=False)
+        df = pd.read_csv(os.path.join(savepath, ('odour_dataset_leff_train_0' + '.csv')))#, index_col=False)
         self.graphs = []
         self.labels = []
         self.labels_set = []
@@ -398,7 +398,7 @@ class OdourDataset_test(DGLDataset):
 
     def process(self):
         savepath = '/content/drive/MyDrive/dgl_hgp_sl/dataset/'
-        df = pd.read_csv(os.path.join(savepath, ('odour_graphs_leff_' + str('test') + '.csv')))
+        df = pd.read_csv(os.path.join(savepath, ('odour_dataset_leff_test_0' + '.csv')))
         self.graphs = []
         self.labels = []
         self.labels_set = []
