b3e8f8729c869c29e597c493c65fff546a84a745
Maadi5 <ma.adithya55@gmail.com>
2022-08-28 16:46:50 +0530

    print preds every 10 epoch

--- odourdataset.py
+++ odourdataset.py
@@ -432,5 +432,66 @@ class OdourDataset_test(DGLDataset):
         # return 9, 0,len(self.labels_set), len(self.graphs)
         return self.num_atom_feat, self.num_bond_feat,len(self.labels_set[0]), len(self.graphs)
 
+
+class OdourDataset_eval(DGLDataset):
+    def __init__(self):
+        super(OdourDataset_eval, self).__init__(name='odour_dataset')
+        # self.all_graphs, self.all_labels = dgl.load_graphs(graphs_path)
+        # self.csv_path = csv_path
+        # self.mode = mode
+        self.atom_featurizer = AtomFeaturizer(
+            allowable_sets={
+                #"symbol": {"B", "Br", "C", "Ca", "Cl", "F", "H", "I", "N", "Na", "O", "P", "S"},
+                "symbol": {"C", "N", "O", "S"},
+                "n_valence": {0, 1, 2, 3, 4, 5, 6},
+                "n_hydrogens": {0, 1, 2, 3, 4},
+                "hybridization": {"s", "sp", "sp2", "sp3"},
+            }
+        )
+
+        self.bond_featurizer = BondFeaturizer(
+            allowable_sets={
+                "bond_type": {"single", "double", "triple", "aromatic"},
+                "conjugated": {True, False},
+            }
+        )
+
+
+    def process(self):
+        savepath = '/content/drive/MyDrive/dgl_hgp_sl/dataset/'
+        df = pd.read_csv(os.path.join(savepath, ('leff_test_8' + '.csv')))
+        self.graphs = []
+        self.labels = []
+        self.labels_set = []
+        for idx, row in df.iterrows():
+            if row['IsomericSMILES'] != '':
+                mol = molecule_from_smiles(row['IsomericSMILES'])
+                # print(list(row))
+                #print(len(list(row)[3:-2]))
+                label = [int(i) for i in list(row)[4:]]
+                atom_features, bond_features, pair_indices, num_nodes = graph_from_molecule(mol, global_node=True)
+                g = create_dgl_graph(pair_indices, num_nodes=num_nodes)
+                g.ndata['features'] = torch.from_numpy(np.array(atom_features, dtype=np.float32))
+                g.edata['features'] = torch.from_numpy(np.array(bond_features, dtype=np.float32))
+                # g = smiles2graph(row['SMILES'])
+                # g.ndata['features'] = torch.tensor(feat_vec(row['SMILES']))
+                self.num_atom_feat = atom_features.shape[1]
+                self.num_bond_feat = bond_features.shape[1]
+                self.graphs.append(g)
+                self.labels.append(label)
+                self.labels_set.append(label)
+        self.labels = torch.LongTensor(self.labels)
+        print ("Batch labels in process",self.labels.shape)
+
+    def __getitem__(self, i):
+        return self.graphs[i], self.labels[i]
+
+    def __len__(self):
+        return len(self.graphs)
+
+    def statistics(self):
+        # return 9, 0,len(self.labels_set), len(self.graphs)
+        return self.num_atom_feat, self.num_bond_feat,len(self.labels_set[0]), len(self.graphs)
+
 if __name__ == "__main__":
     pass
